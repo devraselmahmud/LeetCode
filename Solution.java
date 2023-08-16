9cce7b7ecb7b32e144e6e7eadc2ba5c345b54de4
Md. Rasel Mahmud <20-1254-h@student.aiub.edu>
2023-08-16 08:24:31 -0700

    1480. Running Sum of 1d Array

---
+++ Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    static int[] runningSum(int[] nums){
+        int result = 0;
+        for(int i=0; i<nums.length; i++){
+            result = result + nums[i];
+            nums[i] = result;
+        }
+        return nums;
+    }
+    
+}
